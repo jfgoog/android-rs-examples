f5f591c262fdbc9c845a3a39cf33ee9ba765e7d1
James Farrell <jamesfarrell@google.com>
2022-04-06 10:53:41 -0500

    Download quietly, and remove archives after uncompressing.

--- hello-world-no-studio/download.sh
+++ hello-world-no-studio/download.sh
@@ -13,11 +13,13 @@ fi
 mkdir -p ${PREBUILT}
 cd ${PREBUILT}
 
-curl -O https://download.oracle.com/java/18/latest/jdk-18_macos-aarch64_bin.tar.gz
+curl -s -O https://download.oracle.com/java/18/latest/jdk-18_macos-aarch64_bin.tar.gz
 tar zxf jdk-18_macos-aarch64_bin.tar.gz
+rm -f jdk-18_macos-aarch64_bin.tar.gz
 
-curl -O https://dl.google.com/android/repository/commandlinetools-mac-8092744_latest.zip
-unzip commandlinetools-mac-8092744_latest.zip
+curl -s -O https://dl.google.com/android/repository/commandlinetools-mac-8092744_latest.zip
+unzip -qq commandlinetools-mac-8092744_latest.zip
+rm -f commandlinetools-mac-8092744_latest.zip
 
 export JAVA_HOME=${PREBUILT}/jdk-18.jdk/Contents/Home
 export ANDROID_HOME=${PREBUILT}/android_sdk
@@ -26,5 +28,6 @@ alias sdkmanager="${PREBUILT}/cmdline-tools/bin/sdkmanager --sdk_root=${ANDROID_
 yes | sdkmanager 'platforms;android-32'
 sdkmanager 'ndk;24.0.8215888'
 
-curl -O -L https://services.gradle.org/distributions/gradle-7.4.2-bin.zip
-unzip gradle-7.4.2-bin.zip
+curl -s -O -L https://services.gradle.org/distributions/gradle-7.4.2-bin.zip
+unzip -qq gradle-7.4.2-bin.zip
+rm -f gradle-7.4.2-bin.zip
