2eac6325a61980657d1952fe430619aaf1776c01
James Farrell <jamesfarrell@google.com>
2022-04-07 10:55:45 -0500

    Make scripts work on Linux and Mac.

--- hello-world-no-studio/download.sh
+++ hello-world-no-studio/download.sh
@@ -6,28 +6,51 @@ set -x
 PREBUILT=$PWD/prebuilt
 
 if [[ -e ${PREBUILT} ]]; then
-	echo "${PREBUILT} already exists."
-	exit 1
+  echo "${PREBUILT} already exists."
+  exit 1
 fi
 
 mkdir -p ${PREBUILT}
 cd ${PREBUILT}
 
-curl -s -O https://download.oracle.com/java/18/latest/jdk-18_macos-aarch64_bin.tar.gz
-tar zxf jdk-18_macos-aarch64_bin.tar.gz
-rm -f jdk-18_macos-aarch64_bin.tar.gz
+# Download pages:
+# https://www.oracle.com/java/technologies/downloads/
+# https://developer.android.com/studio/#downloads
+case $(uname -s) in
+  Linux)
+    JDK_URL=https://download.oracle.com/java/18/latest/jdk-18_linux-x64_bin.tar.gz
+    CMDLINE_TOOLS_URL=https://dl.google.com/android/repository/commandlinetools-linux-8092744_latest.zip
+    export JAVA_HOME=${PREBUILT}/jdk-18
+    ;;
+  Darwin)
+    CMDLINE_TOOLS_URL=https://dl.google.com/android/repository/commandlinetools-mac-8092744_latest.zip
+    export JAVA_HOME=${PREBUILT}/jdk-18.jdk/Contents/Home
+    case $(uname -m) in
+      x86_64) JDK_URL=https://download.oracle.com/java/18/latest/jdk-18_macos-x64_bin.tar.gz ;;
+      arm64) JDK_URL=https://download.oracle.com/java/18/latest/jdk-18_macos-aarch64_bin.tar.gz ;;
+    esac
+    ;;
+  *)
+    echo "Unrecognized OS $(uname -s)"
+    exit 1
+    ;;
+esac
 
-curl -s -O https://dl.google.com/android/repository/commandlinetools-mac-8092744_latest.zip
-unzip -qq commandlinetools-mac-8092744_latest.zip
-rm -f commandlinetools-mac-8092744_latest.zip
+curl -s -o jdk.tar.gz ${JDK_URL}
+tar zxf jdk.tar.gz
+rm -f jdk.tar.gz
 
-export JAVA_HOME=${PREBUILT}/jdk-18.jdk/Contents/Home
+curl -s -o commandlinetools.zip ${CMDLINE_TOOLS_URL}
+unzip -qq commandlinetools.zip
+rm -f commandlinetools.zip
+
 export ANDROID_HOME=${PREBUILT}/android_sdk
 alias sdkmanager="${PREBUILT}/cmdline-tools/bin/sdkmanager --sdk_root=${ANDROID_HOME}"
 
 yes | sdkmanager 'platforms;android-32'
 sdkmanager 'ndk;24.0.8215888'
 
+# Download page: https://gradle.org/releases/
 curl -s -O -L https://services.gradle.org/distributions/gradle-7.4.2-bin.zip
 unzip -qq gradle-7.4.2-bin.zip
 rm -f gradle-7.4.2-bin.zip
--- hello-world-no-studio/env.sh
+++ hello-world-no-studio/env.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-PREBUILT=/Users/jamesfarrell/src/android-rs-examples/hello-world-no-studio/prebuilt
-export JAVA_HOME=${PREBUILT}/jdk-18.jdk/Contents/Home
+PREBUILT=$PWD/prebuilt
+case $(uname -s) in
+  Linux) export JAVA_HOME=${PREBUILT}/jdk-18 ;;
+  Darwin) export JAVA_HOME=${PREBUILT}/jdk-18.jdk/Contents/Home ;;
+esac
 export ANDROID_HOME=${PREBUILT}/android_sdk
 alias sdkmanager="${PREBUILT}/cmdline-tools/bin/sdkmanager --sdk_root=${ANDROID_HOME}"
 alias gradle=${PREBUILT}/gradle-7.4.2/bin/gradle
